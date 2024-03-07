b4c013bcedc557a48c1c80f5cf9f364484464e36
Mateus Silva Massariol <mateussilvamassariol@gmail.com>
2024-03-07 14:21:56 -0300

    Front

---
+++ assets/scripts/dados.js
@@ -0,0 +1,353 @@
+var MENU = {
+    "burgers": [
+        {
+            "id": "Athelstan",
+            "img": "./assets/images/cardapio/burguers/athelstan.jpg",
+            "name": "Athelstan",
+            "dsc": "Pão brioche, 160g de Carne, Queijo Gouda, Queijo Cheddar, Gorgonzola e Bacon Defumado.",
+            "price": 28.90
+        }, 
+        {
+            "id": "Balder",
+            "img": "./assets/images/cardapio/burguers/balder.jpg",
+            "name": "Balder",
+            "dsc": "Pão de brioche, Cebola Caramelizada, Alface Americano, Tomate Italiano, 160g de Carne, Queijo Prato e Bacon Defumado.",
+            "price": 28.90
+        },  
+        {
+            "id": "BispoHeahmund",
+            "img": "./assets/images/cardapio/burguers/bispo-heahmund.jpg",
+            "name": "Bispo Heahmund",
+            "dsc": "Pão de brioche, molho verde, Alface Americano, Tomate Italiano, 160g de Carne,cheddar, Queijo Prato, OVO e Bacon Defumado.",
+            "price": 29.90
+        },    
+        {
+            "id": "BjornIronside",
+            "img": "./assets/images/cardapio/burguers/bjorn-hironside.jpg",
+            "name": "Bjorn Ironside",
+            "dsc": "Pão de brioche, molho aioli, 160g de carne, cheddar, Bacon Defumado e requeijão empanado.",
+            "price": 32.90
+        }, 
+        {
+            "id": "DoisX-Burguer",
+            "img": "./assets/images/cardapio/burguers/2x-burger.jpg",
+            "name": "Dois X-Burguer",
+            "dsc": "Pão Brioche,  100g de Carne, Queijo Prata.",
+            "price": 29.90
+        },   
+        {
+            "id": "DuploCheeseBurger",
+            "img": "./assets/images/cardapio/burguers/duplo-x-burger.jpg",
+            "name": "Duplo Cheese Burger",
+            "dsc": "Pão de brioche, 2 carnes de 160g com Queijo Prato.",
+            "price": 31.90
+        },
+        {
+            "id": "Floki",
+            "img": "./assets/images/cardapio/burguers/floki.jpg",
+            "name": "Duplo Cheese Burger",
+            "dsc": "Pão de brioche, ketchup, 160g de carne, Queijo Prato, catupiry, calabresa e cebola crispy.",
+            "price": 29.90
+        },
+        {
+            "id": "Freya",
+            "img": "./assets/images/cardapio/burguers/freya.jpg",
+            "name": "Freya",
+            "dsc": "Pão de brioche, alface americana, Tomate Italiano, cheddar, 160g de carne e Bacon Defumado.",
+            "price": 28.90
+        },
+        {
+            "id": "Gyda",
+            "img": "./assets/images/cardapio/burguers/gyda.jpg",
+            "name": "Gyda",
+            "dsc": "Pão de brioche, 160g de carne e Queijo Prato.",
+            "price": 20.90
+        },
+        {
+            "id": "Hvitserk",
+            "img": "./assets/images/cardapio/burguers/gyda.jpg",
+            "name": "Hvitserk",
+            "dsc": "Pão de brioche, molho de aioli, carne 160G, queijo prato, farofa de bacon e 3 anéis de cebola.",
+            "price": 27.90
+        },
+        {
+            "id": "Ivar",
+            "img": "./assets/images/cardapio/burguers/ivar.jpg",
+            "name": "Ivar",
+            "dsc": "Pão de brioche, molho aioli, 2 carnes de 160g, queijo prato, Bacon Defumado e onion rings.",
+            "price": 27.90
+        },
+        {
+            "id": "Judith",
+            "img": "./assets/images/cardapio/burguers/judith.jpg",
+            "name": "Judith",
+            "dsc": "Pão de brioche, Bastante Molho Americano, Alface Americano, Tomate Italiano, Carne 160g, Queijo Prato e Bacon Defumando.",
+            "price": 25.90
+        },
+        {
+            "id": "Lagertha",
+            "img": "./assets/images/cardapio/burguers/largheta.jpg",
+            "name": "Lagertha",
+            "dsc": "Pão de brioche, Cream Cheese, Alface Americana, Tomate Italiano, 160g de frango, Queijo Prato e Farofa de Bacon.",
+            "price": 24.90
+        },
+        {
+            "id": "Loki",
+            "img": "./assets/images/cardapio/burguers/loki.jpg",
+            "name": "Loki",
+            "dsc": "Pão de brioche, molho aioli, 160 de carne, queijo prato e cebola crispy.",
+            "price": 27.90
+        },
+        {
+            "id": "OVidente",
+            "img": "./assets/images/cardapio/burguers/vidente.jpg",
+            "name": "O Vidente",
+            "dsc": "3 Fatias de pães brioche, MOLHO AMERICANO, 2x carne, 2x QUEIJO PRATO, 2x Alface Americano e Tomate Italiano 2X BACON DEFUMADO",
+            "price": 37.90
+        },
+        {
+            "id": "Odin",
+            "img": "./assets/images/cardapio/burguers/odin.jpg",
+            "name": "Odin",
+            "dsc": "Pão de brioche, 160g de carne, queijo gouda e chutney de abacaxi.",
+            "price": 26.90
+        },
+        {
+            "id": "Ragnar",
+            "img": "./assets/images/cardapio/burguers/ragnar.jpg",
+            "name": "Ragnar",
+            "dsc": "Pão de brioche, molho verde, alface americana, tomate italiano, 160g de carne, cheddar, queijo prato, bacon defumado e cebola crispy.",
+            "price": 31.90
+        },
+        {
+            "id": "ReiEcbert",
+            "img": "./assets/images/cardapio/burguers/rei-ecbert.jpg",
+            "name": "Rei Ecbert",
+            "dsc": "Pão brioche, Geleia de pimenta, Carne 160G, Cream Cheese, Bacon defumado e Cebola Crispy",
+            "price": 29.90
+        },
+        {
+            "id": "ReiHarald",
+            "img": "./assets/images/cardapio/burguers/rei-harald.jpg",
+            "name": "Rei Harald",
+            "dsc": "Pão de brioche, molho aioli, carne 160G, queijo prato, bacon defumado e  150g de Empanado de Cheddar",
+            "price": 31.90
+        },
+        {
+            "id": "Rollo",
+            "img": "./assets/images/cardapio/burguers/rollo.jpg",
+            "name": "Rollo",
+            "dsc": "Pão de brioche, 160g carne, gorgonzola e bacon defumado.",
+            "price": .90
+        },
+        {
+            "id": "Sigurd",
+            "img": "./assets/images/cardapio/burguers/sigurd.jpg",
+            "name": "Sigurd",
+            "dsc": "Pão brioche, Caner Recheada com Queijo, cheddar, bacon e ovo",
+            "price": 29.90
+        },
+        {
+            "id": "Thor",
+            "img": "./assets/images/cardapio/burguers/thor.jpg",
+            "name": "Thor",
+            "dsc": "Pão australiano, cebola caramelizada, 160g de carne, cheddar e Bacon defumado",
+            "price": 28.90
+        },
+        {
+            "id": "Torvi",
+            "img": "./assets/images/cardapio/burguers/torvi.jpg",
+            "name": "Torvi",
+            "dsc": "Pão brioche, Cebola Caramelizada, Carne 160G, Queijo Prato e Bacon Defumado",
+            "price": 26.90
+        },
+        {
+            "id": "Ubbe",
+            "img": "./assets/images/cardapio/burguers/ubbe.jpg",
+            "name": "Ubbe",
+            "dsc": "Pão de brioche, 160G DE CARNE, MOLHO AIOLI, ALFACE AMERICANA, TOMATE ITALIANO,  QUEIJO PRATO, CHUTNEY DE ABACAXI E BACON DEFUMADO",
+            "price": 28.90
+        },
+        {
+            "id": "Valkirias",
+            "img": "./assets/images/cardapio/burguers/valkirias.jpg",
+            "name": "Valkirias",
+            "dsc": "Pão de brioche, 160g de carne, cheddar e farofa de bacon.",
+            "price": 26.90
+        }
+    ],
+    "promocao":[
+        {
+            "id": "Ragnar2x",
+            "img": "./assets/images/cardapio/burguers/2x-ragnar.jpg",
+            "name": "2x Ragnar",
+            "dsc": "Pão de brioche, molho verde, alface americana, tomate italiano, 160g de carne, cheddar, queijo prato, bacon defumado e cebola crispy.",
+            "price": 54.99
+        },
+        {
+            "id": "Judith",
+            "img": "./assets/images/cardapio/burguers/judith.jpg",
+            "name": "Judith",
+            "dsc": "Pão de brioche, Bastante Molho Americano, Alface Americano, Tomate Italiano, Carne 160g, Queijo Prato e Bacon Defumando.",
+            "price": 25.90
+        }
+    ],
+    "molhos":[
+        {
+            "id": "aioli",
+            "img": "./assets/images/cardapio/molhos/aioli.jpg",
+            "name": "Molho Aioli",
+            "price": 3
+        },
+        {
+            "id": "molhoamericano",
+            "img": "./assets/images/cardapio/molhos/aioli.jpg",
+            "name": "Molho Americano",
+            "price": 5
+        },
+        {
+            "id": "barbecue",
+            "img": "./assets/images/cardapio/molhos/barbecue.jpg",
+            "name": "Barbecue",
+            "price": 3
+        },
+        {
+            "id": "molhoverde",
+            "img": "./assets/images/cardapio/molhos/molhoverde.jpg",
+            "name": "Molho Verde",
+            "price": 3
+        }
+    ],
+    "acompanhamentos": [
+        {
+            "id": "BatataCanoaP",
+            "img": "./assets/images/cardapio/acompanhamentos/batatacanoa.jpg",
+            "name": "Batata Canoa P",
+            "dsc": "150g de Batata Canoa.",
+            "price": 10
+        },
+        {
+            "id": "BatataCanoaG",
+            "img": "./assets/images/cardapio/acompanhamentos/batatacanoagrande.jpg",
+            "name": "Batata Canoa G",
+            "dsc": "300g de Batata Canoa.",
+            "price": 20
+        },
+        {
+            "id": "BatataPalitoP",
+            "img": "./assets/images/cardapio/acompanhamentos/batatapalito.jpg",
+            "name": "Batata Palito P",
+            "dsc": "150g de Batata Palito.",
+            "price": 8
+        },
+        {
+            "id": "BatataPalitoG",
+            "img": "./assets/images/cardapio/acompanhamentos/batatapalito.jpg",
+            "name": "Batata Palito G",
+            "dsc": "300g de Batata Palito.",
+            "price": 18
+        },
+        {
+            "id": "BatataValhalla",
+            "img": "./assets/images/cardapio/acompanhamentos/batatavalhalla.jpg",
+            "name": "Batata Valhalla",
+            "dsc": "300g de batata palito, cheddar cremoso e farofa de bacon",
+            "price": 24.90
+        },
+        {
+            "id": "ChickenFrango",
+            "img": "./assets/images/cardapio/acompanhamentos/chickendefrango.jpg",
+            "name": "Chicken de Frango",
+            "dsc": "7 unidades, acompanha um molho barbecue.",
+            "price": 16.90
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "AguaGas",
+            "img": "./assets/images/cardapio/bebidas/aguagas.png",
+            "name": "Água com Gás",
+            "dsc": "",
+            "price": 4
+        },
+        {
+            "id": "AguaSemGas",
+            "img": "./assets/images/cardapio/bebidas/aguasemgas.png",
+            "name": "Água Sem Gás",
+            "dsc": "",
+            "price": 4
+        },
+        {
+            "id": "CocaCola350",
+            "img": "./assets/images/cardapio/bebidas/coca-cola-350.jpg",
+            "name": "Coca-Cola",
+            "dsc": "350ml",
+            "price": 6.5
+        },
+        {
+            "id": "CocaCola350Z",
+            "img": "./assets/images/cardapio/bebidas/coca-cola-zero-350.jpg",
+            "name": "Coca-Cola Zero",
+            "dsc": "350ml",
+            "price": 6.5
+        },
+        {
+            "id": "FantaLaranja350",
+            "img": "./assets/images/cardapio/bebidas/fanta-laranja-350.jpg",
+            "name": "Fanta Laranja",
+            "dsc": "350ml",
+            "price": 6.5
+        },
+        {
+            "id": "FantaUva350",
+            "img": "./assets/images/cardapio/bebidas/fanta-uva-350.jpg",
+            "name": "Fanta Uva",
+            "dsc": "350ml",
+            "price": 6.5
+        },
+        {
+            "id": "FantaGuarana350",
+            "img": "./assets/images/cardapio/bebidas/fanta-guarana-350.jpg",
+            "name": "Guaraná",
+            "dsc": "350ml",
+            "price": 6.5
+        },
+        {
+            "id": "FantaGuarana2l",
+            "img": "./assets/images/cardapio/bebidas/fanta-guarana-2-litros.jpg",
+            "name": "Fanta Guaraná",
+            "dsc": "2lts",
+            "price": 13 
+        },
+        {
+            "id": "FantaLaranja2l",
+            "img": "./assets/images/cardapio/bebidas/fanta-laranja-2lts.jpg",
+            "name": "Fanta Laranja",
+            "dsc": "2lts",
+            "price": 13
+        },
+        {
+            "id": "FantaUva2l",
+            "img": "./assets/images/cardapio/bebidas/fanta-uva-2-litros.jpg",
+            "name": "Fanta Uva",
+            "dsc": "2lts",
+            "price": 13
+        },
+        {
+            "id": "Coca2l",
+            "img": "./assets/images/cardapio/bebidas/coca-cola-2-litros.jpg",
+            "name": "Coca-Cola",
+            "dsc": "2lts",
+            "price": 13
+        }
+    ],
+    "sobremesas": [
+        {
+            "id": "pudim",
+            "img": "./assets/images/cardapio/sobremesas/pudim.jpg",
+            "name": "Pudim",
+            "dsc": "",
+            "price": 10
+        }
+    ] 
+}
